ebca4de3145ac6815d8286cb6042c1b08964733c
Alex Simpson <alxsimpson@gmail.com>
2020-07-30 15:55:44 +1000

    Updates

---
+++ precache-manifest.5295f5be69045082690ed73a48e769d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b48af72ce35222449ba3e18ea1ace65",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "50bbcda43e4b0a9fb29d",
+    "url": "/jammming/static/css/main.b7346e01.chunk.css"
+  },
+  {
+    "revision": "b0f597be5bf5280f10d7",
+    "url": "/jammming/static/js/2.87f56ebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.87f56ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bbcda43e4b0a9fb29d",
+    "url": "/jammming/static/js/main.f563634f.chunk.js"
+  },
+  {
+    "revision": "d54c33d011ceab8fe7ea",
+    "url": "/jammming/static/js/runtime-main.af6152f4.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
